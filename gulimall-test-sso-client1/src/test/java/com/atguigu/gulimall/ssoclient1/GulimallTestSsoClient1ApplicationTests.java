3b33955d3fc5d68499b64843ace68c281c724771
liuzhuopeng <lzhuopeng@163.com>
2022-04-08 15:18:47 +0800

    单点登录

---
+++ gulimall-test-sso-client1/src/test/java/com/atguigu/gulimall/ssoclient1/GulimallTestSsoClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.ssoclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallTestSsoClient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
